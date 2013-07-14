e45ed5456f0977e67cbfe0106be7dee9936d23a8
Misty De Meo <mistydemeo@gmail.com>
2013-07-13 19:53:06 -0700

    ProjectileShooter: shoot right by default

--- Assets/Scripts/ProjectileShooter.js
+++ Assets/Scripts/ProjectileShooter.js
@@ -6,7 +6,7 @@ var timeBetweenWaves : float = 0.7;
 enum direction { left, right };
 
 function Start () {
-	FireSpreads(1.0, 6, 3, direction.left);
+	FireSpreads(1.0, 6, 3, direction.right);
 }
 
 function Update () {
