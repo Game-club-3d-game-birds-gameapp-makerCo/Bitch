92c4be7e979a32aa929f76a1ef8f3a21443743b6
Misty De Meo <mistydemeo@gmail.com>
2013-07-13 23:06:30 -0700

    BulletMovement: Don't hardcode travel direction

--- Assets/Scripts/BulletMovement.js
+++ Assets/Scripts/BulletMovement.js
@@ -3,12 +3,14 @@
 var velocityX : float = 1.0;
 var velocityZ : float = 1.0;
 var timeToLive : float = 9.0;
+var direction = -Vector3.forward;
+var player : Transform;
 
 function Start () {
 	Destroy(gameObject, timeToLive);
 }
 
 function Update () {
-	transform.Translate(-Vector3.forward * Time.deltaTime * velocityZ);
+	transform.Translate(direction * Time.deltaTime * velocityZ);
 	transform.Translate(Vector3.right * Time.deltaTime * velocityX);
 }
